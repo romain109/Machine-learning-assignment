76eaad3ce68e7b5b3feae103baf23c96abd6f86f
romain molliet <romainalexandre@hotmail.fr>
2020-05-24 11:40:34 +0200

    code

--- machine learning assignment.R
+++ machine learning assignment.R
@@ -1,12 +1,15 @@
-data <- read.csv("./data/training.csv")
-valid <- read.csv("./data/Valid.csv")
+library(caret)
+
+data <- read.csv("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv")
+valid <- read.csv("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv")
 
 
-inTrain <- createDataPartition(y=data$classe,
-                               p=0.7, list=FALSE)
+inTrain <- createDataPartition(y=data$classe,p=0.7, list=FALSE)
 training <- data[inTrain,]
 testing <- data[-inTrain,]
 dim(training); dim(testing)
+
+
 columns <- c(8:11, 37:49,60:68,84:86, 102, 113:124,140,151:160)
 training <- training[,columns]
 testing <- testing[,columns]
@@ -15,4 +18,5 @@ set.seed(123)
 
 modFitRf <- train(classe~.,data=training, method="rf")
 prediction_rf <- predict(modFitRf,newdata=testing)
-confusionMatrix(prediction_rf,testing$classe)
+
+
